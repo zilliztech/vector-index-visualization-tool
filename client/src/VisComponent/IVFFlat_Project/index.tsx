708e18c587bcb1d37a539e9d6d38634e48779f0e
min.tian <min.tian.cn@gmail.com>
2021-11-19 18:37:30 +0800

    ..

--- client/src/VisComponent/IVFFlat_Project/index.tsx
+++ client/src/VisComponent/IVFFlat_Project/index.tsx
@@ -268,20 +268,34 @@ const View = observer(({ nodesData }: { nodesData: INodesData[] }) => {
 
   return (
     <svg id={svgId} width="100%" height="100%">
-      {nodesData.map((node, i) => (
-        <circle
-          key={node.id}
-          id={node.id}
-          cx={x[level](node.projection[level][0])}
-          cy={y[level](node.projection[level][1])}
-          r={i === 1000 ? 20 : 3}
-          fill={node.color[level]}
-          opacity={node.opacity[level]}
-          style={{
-            transition: `${node.transition[level]}`,
-          }}
-        />
-      ))}
+      {nodesData.map((node, i) =>
+        node.type[level] === NodeType.Target ? (
+          <path
+            d={getStarPath(
+              x[level](node.projection[level][0]),
+              y[level](node.projection[level][1]),
+              30
+            )}
+            fill="red"
+            style={{
+              transition: `all 1s ease`,
+            }}
+          />
+        ) : (
+          <circle
+            key={node.id}
+            id={node.id}
+            cx={x[level](node.projection[level][0])}
+            cy={y[level](node.projection[level][1])}
+            r={i === 1000 ? 20 : 3}
+            fill={node.color[level]}
+            opacity={node.opacity[level]}
+            style={{
+              transition: `${node.transition[level]}`,
+            }}
+          />
+        )
+      )}
       {showImage && (
         <g>
           {showImageNodes.map((node) => (
@@ -330,3 +344,39 @@ const View = observer(({ nodesData }: { nodesData: INodesData[] }) => {
     </svg>
   );
 });
+
+const getStarPath = (x: number, y: number, width: number) => {
+  x = x - width / 2;
+  y = y - width / 2 / Math.tan((72 / 180) * Math.PI);
+  const coordB = {
+    x0: x + width,
+    y0: y,
+  };
+  const coordD = {
+    x0:
+      x +
+      width * Math.sin((36 / 180) * Math.PI) * Math.tan((18 / 180) * Math.PI),
+    y0: y + width * Math.sin((36 / 180) * Math.PI),
+  };
+  const coordA = {
+    x0: x + width / 2,
+    y0: y - (width / 2) * Math.tan((36 / 180) * Math.PI),
+  };
+  const coordC = {
+    x0: x + width * Math.cos((36 / 180) * Math.PI),
+    y0: y + width * Math.sin((36 / 180) * Math.PI),
+  };
+  //绘制星星
+  return `M${x},${y} L${coordB.x0},${coordB.y0} L${coordD.x0},${coordD.y0} L${coordA.x0},${coordA.y0} L${coordC.x0},${coordC.y0} Z`;
+};
+
+const getPolygon = (x: number, y: number, r: number, n: number) => {
+  const stepAngle = (2 * Math.PI) / n;
+  const points = d3
+    .range(n)
+    .map(
+      (i) =>
+        `${x + r * Math.sin(stepAngle * i)},${y - r * Math.cos(stepAngle * i)}`
+    );
+  return `M${points.join("L")}`;
+};
